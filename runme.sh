c1811febbf5e4bc3221d98e41b23d46ffa3bf9ac
tipodice <30523181+tipodice@users.noreply.github.com>
2025-08-23 15:20:52 -0300

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -18,6 +18,8 @@ git clone https://github.com/rgthree/rgthree-comfy.git &&
 
 git clone https://github.com/kijai/ComfyUI-KJNodes.git &&
 
+git clone https://github.com/yolain/ComfyUI-Easy-Use.git &&
+
 wget -P /workspace/ComfyUI/models/loras/ https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/loras/wan2.2_i2v_lightx2v_4steps_lora_v1_high_noise.safetensors &&
 
 wget -P /workspace/ComfyUI/models/loras/ https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/loras/wan2.2_i2v_lightx2v_4steps_lora_v1_low_noise.safetensors && 
